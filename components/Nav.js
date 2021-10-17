a7e0e911dfcb38b6d7d2d4ba7892194d706c8e24
Harshal <harshal.2797@gmail.com>
2021-10-18 00:50:10 +0530

    Partially working results components

--- components/Nav.js
+++ components/Nav.js
@@ -9,7 +9,7 @@ function Nav() {
         {Object.entries(requests).map(([key, { title, url }]) => (
           <h2
             key={key}
-            onClick={() => router.push(`/?${key}`)}
+            onClick={() => router.push(`/?genre=${key}`)}
             className="last:pr-24 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500"
           >
             {title}
